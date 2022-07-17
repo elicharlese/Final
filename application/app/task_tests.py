42e78d7413ee6465e6d3f2cdbbab02cedf794fe7
elicharlese <elicharles.e@gmail.com>
2022-07-17 13:43:59 -0700

    Manual 	new file:   Instructions/7_1.md 	new file:   Instructions/7_10.md 	renamed:    application/app/functions.py -> application/app/extra_credit.py 	renamed:    application/app/main.py -> application/app/task_functions.py 	new file:   application/app/task_system.py 	new file:   application/app/task_tests.py 	modified:   application/src/app.js 	modified:   application/src/index.html 	modified:   application/styles/main.css 	new file:   application/styles/main.scss

---
+++ application/app/task_tests.py
@@ -0,0 +1,13 @@
+from task_functions import *
+
+### Use the assert statements to check that your code returns the expected value and is of a correct type.
+# Use it especially to test the edge cases
+# (e.g., when to return -1 or 0, what to do when the input is empty).
+
+### Test the valid inputs ###
+# arg1 = stopping_volume
+# arg2 = signal_1
+# result = trigger_accumulation(stopping_volume, signal_1)
+# expected_result = "zero"
+# print(f"--> trigger_accumulation({arg1, arg2}) returned `{result}`\n")
+# assert result == expected_result
